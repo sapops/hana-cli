56a95adc5edfdccb82b89683b397cd79a59b594a
Petr Plenkov <petr.plenkov@booking.com>
2023-03-07 19:52:53 +0000

    migrate version

--- packages/hana2cds/jest.config.ts
+++ packages/hana2cds/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'hana2cds',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/hana2cds',
